76156b93d5898beed1016a9a5a7d1699c98475e3
Zhiang Zhang <zhangzhizza@gmail.com>
2017-03-28 21:47:50 -0400

    Eplus forecast env first working version

--- src/eplus_env_test.py
+++ src/eplus_env_test.py
@@ -1,11 +1,37 @@
 import gym
-import core.eplus-env.eplus_env
+import eplus_env
+import numpy as np
 
-env = gym.make('Eplus-v0');
-print ('reset')
-env.reset()
+"""
+# Noforecast env
+env = gym.make('Eplus-noforecast-v0');
+is_terminal = env.reset()[-1];
 
-for _ in range(500):
-    env.step([]);
+while is_terminal != True:
+    is_terminal = env.step([])[-1];
+
+print ('resetting...................')
+is_terminal = env.reset()
+
+while is_terminal != True:
+    is_terminal = env.step([])[-1];
+
+env.end_env()
+"""
+# Forecast env
+env = gym.make('Eplus-forecast-v0');
+time, data, forecast, is_terminal = env.reset();
+print (np.array(forecast)[:,0]);
+
+while is_terminal != True:
+    time, data, forecast, is_terminal = env.step([]);
+    print (np.array(forecast)[:,0]);
+
+print ('resetting...................')
+time, data, forecast, is_terminal = env.reset()
+print (np.array(forecast)[:,0]);
+while is_terminal != True:
+    time, data, forecast, is_terminal = env.step([]);
+    print (np.array(forecast)[:,0]);
 
 env.end_env()
