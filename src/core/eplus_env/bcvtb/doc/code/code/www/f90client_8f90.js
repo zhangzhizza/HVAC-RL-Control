3fcc4add7f71f195e7db3fa71d58a3b60b488925
zhangzhizza <zhangzhizza@gmail.com>
2017-03-30 17:18:39 -0400

    Delete unwanted files.

--- src/core/eplus_env/bcvtb/doc/code/code/www/f90client_8f90.js
+++
@@ -1,4 +0,0 @@
-var f90client_8f90 =
-[
-    [ "first", "f90client_8f90.html#ac2997288c0f5271b839554d4731f6086", null ]
-];
--- src/core/eplus_env/bcvtb/doc/code/code/www/namespacebacnet_1_1actor.js
+++
@@ -1,5 +0,0 @@
-var namespacebacnet_1_1actor =
-[
-    [ "BACnetReader", "classbacnet_1_1actor_1_1BACnetReader.html", "classbacnet_1_1actor_1_1BACnetReader" ],
-    [ "BACnetWriter", "classbacnet_1_1actor_1_1BACnetWriter.html", "classbacnet_1_1actor_1_1BACnetWriter" ]
-];
