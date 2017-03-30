d3705a6cd522e87536ee9827eb9fa75a28007b47
zhangzhizza <zhangzhizza@gmail.com>
2017-03-30 17:18:39 -0400

    Delete unwanted files.

--- src/core/eplus_env/bcvtb/doc/code/code/www/namespacebacnet.js
+++
@@ -1,5 +0,0 @@
-var namespacebacnet =
-[
-    [ "actor", "namespacebacnet_1_1actor.html", "namespacebacnet_1_1actor" ],
-    [ "util", "namespacebacnet_1_1util.html", "namespacebacnet_1_1util" ]
-];
--- src/core/eplus_env/bcvtb/lib/ptII/ptolemy/configs/test/README.txt
+++
@@ -1,2 +0,0 @@
-$Id: README.txt 61981 2011-09-13 23:43:49Z cxh $
-See package.html
