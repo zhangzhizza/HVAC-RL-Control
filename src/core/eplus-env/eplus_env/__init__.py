4ff6764083eba8b76065025ba614447fdeac5a99
Zhiang Zhang <zhangzhizza@gmail.com>
2017-04-28 15:04:25 -0400

    a3c evaluation

--- src/core/eplus-env/eplus_env/__init__.py
+++ src/core/eplus-env/eplus_env/__init__.py
@@ -1,5 +1,6 @@
 from gym.envs.registration import register
 import os
+import fileinput
 
 FD = os.path.dirname(os.path.realpath(__file__));
 
@@ -31,3 +32,44 @@ register(
             'bcvtb_path':FD + '/envs/bcvtb/',
             'variable_path':FD + '/envs/variables.cfg',
             'idf_path':FD + '/envs/5ZoneAutoDXVAV_eval.idf'});
+    
+# Replace some schedule file path in 5ZoneAutoDXVAV_eval.idf with the 
+# absolute path
+sch_path_dict = {'S1_Office_-_Private People Schedule': FD + '/envs/stochastic_occup.csv',
+                 'S2_Office_-_Private People Schedule': FD + '/envs/stochastic_occup.csv',
+                 'S3_Office_-_Private People Schedule': FD + '/envs/stochastic_occup.csv',
+                 'S4_Office_-_Private People Schedule': FD + '/envs/stochastic_occup.csv',
+                 'S1_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip.csv',
+                 'S2_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip.csv',
+                 'S3_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip.csv',
+                 'S4_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip.csv'};
+contents = None;
+with open(FD + '/envs/5ZoneAutoDXVAV_eval.idf', 'r', encoding = 'ISO-8859-1') as idf:
+    contents = idf.readlines();
+    remember_str = None;
+    remember_idx = -1;
+    schedule_on = False;
+    i = 0;
+    for line in contents:
+        effectiveContent = line.strip().split('!')[0] # Ignore contents after '!'
+        effectiveContent = effectiveContent.strip().split(',')[0]
+                                                            # Remove tailing ','
+        if effectiveContent == 'Schedule:File':
+            schedule_on = True;
+        if effectiveContent in['S1_Office_-_Private People Schedule',
+                                'S2_Office_-_Private People Schedule',
+                                'S3_Office_-_Private People Schedule', 
+                                'S4_Office_-_Private People Schedule',
+                                'S1_Office_-_Private Equip Schedule',
+                                'S2_Office_-_Private Equip Schedule',
+                                'S3_Office_-_Private Equip Schedule',
+                                'S4_Office_-_Private Equip Schedule']:
+            if schedule_on:
+                remember_str = effectiveContent;
+                remember_idx = i + 2; 
+        if i == remember_idx:
+            contents[i] = sch_path_dict[remember_str] + ', !- File Name-eval_env' + '\n';
+            schedule_on = False;
+        i += 1;
+with open(FD + '/envs/5ZoneAutoDXVAV_eval.idf', 'w', encoding = 'ISO-8859-1') as idf:
+    idf.writelines(contents);
