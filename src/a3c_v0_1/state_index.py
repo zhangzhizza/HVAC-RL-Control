152987d2193f3239e2b2b4ac578ae8c65e48fd71
zhiangz <zhiangzhizza@gmail.com>
2017-05-09 17:03:45 -0400

    Add two new environments, and new version of a3c v0.1 which works for the v1 environment and with dropout regularization.

---
+++ src/a3c_v0_1/state_index.py
@@ -0,0 +1,17 @@
+"""
+State feature index.
+"""
+OADT_RAW_IDX = 0;
+OARH_RAW_IDX = 1;
+WS_RAW_IDX = 2;
+WD_RAW_IDX = 3;
+DFS_RAW_IDX = 4;
+DIS_RAW_IDX = 5;
+HTSP_RAW_IDX = 6;
+CLSP_RAW_IDX = 7;
+ZADT_RAW_IDX = 8;
+ZARH_RAW_IDX = 9;
+ZPPD_RAW_IDX = 10;
+ZPCT_RAW_IDX = 11;
+HVACE_RAW_IDX = 12;
+ZN_OB_NUM = 6 # Eight elements are related to zone observation
--- src/core/eplus-env/eplus_env/__init__.py
+++ src/core/eplus-env/eplus_env/__init__.py
@@ -10,9 +10,19 @@ register(
     kwargs={'eplus_path':FD + '/envs/EnergyPlus-8-6-0/',
             'weather_path':FD + '/envs/pittsburgh.epw',
             'bcvtb_path':FD + '/envs/bcvtb/',
-            'variable_path':FD + '/envs/variables.cfg',
+            'variable_path':FD + '/envs/variables_v0.cfg',
             'idf_path':FD + '/envs/5ZoneAutoDXVAV.idf',
             'env_name': 'Eplus-v0'});
+
+register(
+    id='Eplus-v1',
+    entry_point='eplus_env.envs:EplusEnv',
+    kwargs={'eplus_path':FD + '/envs/EnergyPlus-8-6-0/',
+            'weather_path':FD + '/envs/pittsburgh.epw',
+            'bcvtb_path':FD + '/envs/bcvtb/',
+            'variable_path':FD + '/envs/variables_v1.cfg',
+            'idf_path':FD + '/envs/5ZoneAutoDXVAV_v1.idf',
+            'env_name': 'Eplus-v1'});
 
 register(
     id='Eplus-forecast-v0',
@@ -20,7 +30,7 @@ register(
     kwargs={'eplus_path':FD + '/envs/EnergyPlus-8-6-0/',
             'weather_path':FD + '/envs/pittsburgh.epw',
             'bcvtb_path':FD + '/envs/bcvtb/',
-            'variable_path':FD + '/envs/variables.cfg',
+            'variable_path':FD + '/envs/variables_v0.cfg',
             'idf_path':FD + '/envs/5ZoneAutoDXVAV.idf',
             'incl_forecast': True,
             'forecast_step': 36,
@@ -32,9 +42,19 @@ register(
     kwargs={'eplus_path':FD + '/envs/EnergyPlus-8-6-0/',
             'weather_path':FD + '/envs/pennstate.epw',
             'bcvtb_path':FD + '/envs/bcvtb/',
-            'variable_path':FD + '/envs/variables.cfg',
+            'variable_path':FD + '/envs/variables_v0.cfg',
             'idf_path':FD + '/envs/5ZoneAutoDXVAV_eval.idf',
             'env_name': 'Eplus-eval-v0'});
+
+register(
+    id='Eplus-eval-v1',
+    entry_point='eplus_env.envs:EplusEnv',
+    kwargs={'eplus_path':FD + '/envs/EnergyPlus-8-6-0/',
+            'weather_path':FD + '/envs/pennstate.epw',
+            'bcvtb_path':FD + '/envs/bcvtb/',
+            'variable_path':FD + '/envs/variables_v1.cfg',
+            'idf_path':FD + '/envs/5ZoneAutoDXVAV_eval_v1.idf',
+            'env_name': 'Eplus-eval-v1'});
     
 register(
     id='Eplus-eval-multiagent-v0',
@@ -46,80 +66,57 @@ register(
             'idf_path':FD + '/envs/5ZoneAutoDXVAV_eval_multiagent.idf',
             'env_name': 'Eplus-eval-multiagent-v0'});
 
+def setSchedulePath(sch_path_dict, tgtIDFPath):
+    """
+    Set the abosolute path for the Schedule:File.
+    """
+    contents = None;
+    with open(tgtIDFPath, 'r', encoding = 'ISO-8859-1') as idf:
+        contents = idf.readlines();
+        remember_str = None;
+        remember_idx = -1;
+        schedule_on = False;
+        i = 0;
+        for line in contents:
+            effectiveContent = line.strip().split('!')[0] # Ignore contents after '!'
+            effectiveContent = effectiveContent.strip().split(',')[0]
+                                                            # Remove tailing ','
+            if effectiveContent == 'Schedule:File':
+                schedule_on = True;
+            if effectiveContent in list(sch_path_dict.keys()):
+                if schedule_on:
+                    remember_str = effectiveContent;
+                    remember_idx = i + 2; 
+            if i == remember_idx:
+                contents[i] = sch_path_dict[remember_str] + ', !- File Name-eval_env' + '\n';
+                schedule_on = False;
+            i += 1;
+    with open(tgtIDFPath, 'w', encoding = 'ISO-8859-1') as idf:
+        idf.writelines(contents);
+
 # Replace some schedule file path in 5ZoneAutoDXVAV_eval.idf with the 
 # absolute path
 sch_path_dict = {'S1_Office_-_Private People Schedule': FD + '/envs/stochastic_occup.csv',
                  'S2_Office_-_Private People Schedule': FD + '/envs/stochastic_occup.csv',
                  'S3_Office_-_Private People Schedule': FD + '/envs/stochastic_occup.csv',
                  'S4_Office_-_Private People Schedule': FD + '/envs/stochastic_occup.csv',
+                 'S5_Office_-_Private People Schedule': FD + '/envs/stochastic_occup.csv',
                  'S1_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip.csv',
                  'S2_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip.csv',
                  'S3_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip.csv',
-                 'S4_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip.csv'};
-contents = None;
-with open(FD + '/envs/5ZoneAutoDXVAV_eval.idf', 'r', encoding = 'ISO-8859-1') as idf:
-    contents = idf.readlines();
-    remember_str = None;
-    remember_idx = -1;
-    schedule_on = False;
-    i = 0;
-    for line in contents:
-        effectiveContent = line.strip().split('!')[0] # Ignore contents after '!'
-        effectiveContent = effectiveContent.strip().split(',')[0]
-                                                            # Remove tailing ','
-        if effectiveContent == 'Schedule:File':
-            schedule_on = True;
-        if effectiveContent in['S1_Office_-_Private People Schedule',
-                                'S2_Office_-_Private People Schedule',
-                                'S3_Office_-_Private People Schedule', 
-                                'S4_Office_-_Private People Schedule',
-                                'S1_Office_-_Private Equip Schedule',
-                                'S2_Office_-_Private Equip Schedule',
-                                'S3_Office_-_Private Equip Schedule',
-                                'S4_Office_-_Private Equip Schedule']:
-            if schedule_on:
-                remember_str = effectiveContent;
-                remember_idx = i + 2; 
-        if i == remember_idx:
-            contents[i] = sch_path_dict[remember_str] + ', !- File Name-eval_env' + '\n';
-            schedule_on = False;
-        i += 1;
-with open(FD + '/envs/5ZoneAutoDXVAV_eval.idf', 'w', encoding = 'ISO-8859-1') as idf:
-    idf.writelines(contents);
-
-# Replace some schedule file path in SmallOfficePost1980_base_v1.idf with the 
+                 'S4_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip.csv',
+                 'S5_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip.csv'};
+setSchedulePath(sch_path_dict, FD + '/envs/5ZoneAutoDXVAV_eval.idf');
+# Replace some schedule file path in 5ZoneAutoDXVAV_eval_v1.idf with the 
 # absolute path
-sch_path_dict = {'ZN_0': FD + '/envs/occupant_base_v1.csv',
-                 'ZN_1': FD + '/envs/occupant_base_v1.csv',
-                 'ZN_2': FD + '/envs/occupant_base_v1.csv',
-                 'ZN_3': FD + '/envs/occupant_base_v1.csv',
-                 'ZN_4': FD + '/envs/occupant_base_v1.csv',
-                 'MinOA_Sched': FD + '/envs/occupant_base_v1.csv'};
-contents = None;
-with open(FD + '/envs/SmallOfficePost1980_base_v1.idf', 'r', encoding = 'ISO-8859-1') as idf:
-    contents = idf.readlines();
-    remember_str = None;
-    remember_idx = -1;
-    schedule_on = False;
-    i = 0;
-    for line in contents:
-        effectiveContent = line.strip().split('!')[0] # Ignore contents after '!'
-        effectiveContent = effectiveContent.strip().split(',')[0]
-                                                            # Remove tailing ','
-        if effectiveContent == 'Schedule:File':
-            schedule_on = True;
-        if effectiveContent in['ZN_0',
-                                'ZN_1',
-                                'ZN_2', 
-                                'ZN_3',
-                                'ZN_4',
-                                'MinOA_Sched']:
-            if schedule_on:
-                remember_str = effectiveContent;
-                remember_idx = i + 2; 
-        if i == remember_idx:
-            contents[i] = sch_path_dict[remember_str] + ', !- File Name-env-v1' + '\n';
-            schedule_on = False;
-        i += 1;
-with open(FD + '/envs/SmallOfficePost1980_base_v1.idf', 'w', encoding = 'ISO-8859-1') as idf:
-    idf.writelines(contents);
+sch_path_dict = {'S1_Office_-_Private People Schedule': FD + '/envs/stochastic_occup_v1.csv',
+                 'S2_Office_-_Private People Schedule': FD + '/envs/stochastic_occup_v1.csv',
+                 'S3_Office_-_Private People Schedule': FD + '/envs/stochastic_occup_v1.csv',
+                 'S4_Office_-_Private People Schedule': FD + '/envs/stochastic_occup_v1.csv',
+                 'S5_Office_-_Private People Schedule': FD + '/envs/stochastic_occup_v1.csv',
+                 'S1_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip_v1.csv',
+                 'S2_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip_v1.csv',
+                 'S3_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip_v1.csv',
+                 'S4_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip_v1.csv',
+                 'S5_Office_-_Private Equip Schedule': FD + '/envs/stochastic_equip_v1.csv'};
+setSchedulePath(sch_path_dict, FD + '/envs/5ZoneAutoDXVAV_eval_v1.idf');
