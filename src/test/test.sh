2a0be9661aae810fdd67340d8242a0fb2a18532a
Zhiang Zhang <zhangzhizza@gmail.com>
2017-06-01 15:22:38 -0400

    Add ipython notebook for drawing.

--- src/test/test.sh
+++ src/test/test.sh
@@ -1,3 +1,3 @@
 python ../a3c_eplus_v0.1.py --window_len 24 --e_weight 0.6 --p_weight 0.4 --ppd_penalty_limit 0.15 --action_space default --eval_epi_num 1 --dropout_prob 0.0 \
 --is_warm_start True --model_dir ../a3c-res-v0.1/case20-2/a3c-res-v0.1/Eplus-v1-run1/model_data/model.ckpt-15000000 --job_mode Test \
---test_env Eplus-multiagent-v1 --agent_num 5
+--test_env Eplus-eval-v1 --agent_num 1 --test_mode Single
