b36f4f641426c7a9efb12bcef83110677e3213c3
Zhiang Zhang <zhangzhizza@gmail.com>
2017-11-01 10:13:55 -0400

    Finished 31 32 part 1.

---
+++ src/iw_ctrl_runs/32/run.sh
@@ -0,0 +1,4 @@
+python ../../a3c_eplus_iw_v0.1.py --env IW-v5702 --max_interactions 15000000 --window_len 1 --state_dim 13 --num_threads 16 \
+--action_space iw_af5_1 --save_freq 500000 --eval_freq 250000 --job_mode Train \
+--test_env IW-eval-v5702 --err_penalty_scl 0.30 --act_func 5 --reward_func 10 --init_e 0.0 --rwd_e_para 0.5 --rwd_p_para 0.5 \
+--h_regu_frac 0.1
