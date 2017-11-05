8953c1595e83c98b4d821f1822b8ecb1009c9031
zhiangz <zhangzhizza@gmail.com>
2017-11-05 16:06:07 -0500

    Finished case 31 conts

---
+++ src/iw_ctrl_runs/31/run_cont1_cont1_2_cont1.sh
@@ -0,0 +1,4 @@
+python ../../a3c_eplus_iw_v0.1.py --env IW-v570202 --max_interactions 3000000 --window_len 12 --state_dim 13 --num_threads 16 \
+--action_space iw_af5_1 --save_freq 500000 --eval_freq 250000 --job_mode Train \
+--test_env IW-eval-v570202 --err_penalty_scl 0.15 --act_func 6 --reward_func 10 --init_e 0.0 --rwd_e_para 0.9 --rwd_p_para 0.0 \
+--h_regu_frac 0.01 --is_warm_start True --model_dir a3c-res-v0.1/IW-v570202-run5/model_data/model.ckpt-12000000
