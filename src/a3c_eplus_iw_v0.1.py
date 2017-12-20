2650557dc2f1ee2151ba6bbe7b97a16326e991eb
Zhiang Zhang <zhangzhizza@gmail.com>
2017-12-20 09:47:52 -0500

    new case added 49 50 51 52

--- src/a3c_eplus_iw_v0.1.py
+++ src/a3c_eplus_iw_v0.1.py
@@ -20,8 +20,10 @@ def main():
     # Common args
     parser = get_args();
     # Specific args
-    parser.add_argument('--err_penalty_scl', default=13.0, type=float,
-                        help='Scale the IAT temperature difference error, default is 13.0')
+    parser.add_argument('--err_penalty_scl', default=0.15, type=float,
+                        help='Scale PPD penalty, default is 0.15.')
+    parser.add_argument('--violation_penalty_scl', default=10.0, type=float,
+                        help='Scale temperature setpoint violation error, default is 10.0.')
     parser.add_argument('--act_func', default='1', type=str,
                         help='The action function corresponding to the action space, default is 1, '
                              'corresponding to the actions space iw_1')
@@ -29,7 +31,7 @@ def main():
     args = parser.parse_args();
     # Prepare case specific args
     reward_func = reward_func_dict[args.reward_func]
-    rewardArgs = [args.err_penalty_scl];
+    rewardArgs = [args.err_penalty_scl, args.violation_penalty_scl];
     action_func = act_func_dict[args.act_func][0];
     action_limits = act_func_dict[args.act_func][1];
     raw_state_process_func = raw_state_process_iw;
