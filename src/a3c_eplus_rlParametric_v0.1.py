7f8824ae47b033101e9012c539eaa8e9958d777c
zhiangz <zhangzhizza@gmail.com>
2018-07-25 23:25:49 -0400

    fix bug

--- src/a3c_eplus_rlParametric_v0.1.py
+++ src/a3c_eplus_rlParametric_v0.1.py
@@ -28,6 +28,7 @@ def main():
                         help='The action function corresponding to the action space, default is cslDxActCool_1')
     parser.add_argument('--reward_func', default='cslDxCool_1', type=str)
     parser.add_argument('--metric_func', default='cslDxCool_1', type=str)
+    parser.add_argument('--raw_state_prcs_func', default='cslDx_1', type=str)
     args = parser.parse_args();
     # Prepare case specific args
     reward_func = reward_func_dict[args.reward_func]
@@ -37,9 +38,10 @@ def main():
     train_action_limits = act_func_dict[args.train_act_func][1];
     eval_action_func = act_func_dict[args.eval_act_func][0];
     eval_action_limits = act_func_dict[args.eval_act_func][1];
-    raw_state_process_func = raw_state_process_cslDx_1;
+    raw_state_process_func = raw_state_process_map[args.raw_state_prcs_func][0];
+    raw_stateLimit_process_func = raw_state_process_map[args.raw_state_prcs_func][1];
     effective_main(args, reward_func, rewardArgs, metric_func, train_action_func, eval_action_func, train_action_limits, 
-                    eval_action_limits, raw_state_process_func);
+                    eval_action_limits, raw_state_process_func, raw_stateLimit_process_func);
         
 
 if __name__ == '__main__':
