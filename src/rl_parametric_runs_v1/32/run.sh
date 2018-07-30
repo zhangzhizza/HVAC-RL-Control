fe09ecccaddb87c0d3e40cb7b5e3b5d9401be843
zhiangz <zhangzhizza@gmail.com>
2018-07-30 02:01:09 -0400

    fix bug in run of case 32

--- src/rl_parametric_runs_v1/32/run.sh
+++ src/rl_parametric_runs_v1/32/run.sh
@@ -1,4 +1,4 @@
-python ../../a3c_eplus_rlParametric_v0.1.py --env Model1-Cool-v1 --max_interactions 10000000 --window_len 3 --state_dim 49 --num_threads 16 \
+python ../../a3c_eplus_rlParametric_v0.1.py --env Model1-Cool-v1 --max_interactions 10000000 --window_len 3 --state_dim 6 --num_threads 16 \
 --action_space cslDxCool_1 --save_freq 500000 --eval_freq 250000 --job_mode Train \
 --test_env Model1-Test-Cool-v1 --train_act_func cslDxActCool_1 --eval_act_func cslDxActCool_1 --reward_func cslDxCool_2 --metric_func cslDxCool_2 --init_e 0.0 --rwd_e_para 1.0 --rwd_p_para 1.0 \
 --h_regu_frac 0.1 --forecast_dim 0 --rmsprop_decay 0.99 --rmsprop_momet 0.0 --train_freq 5 \
